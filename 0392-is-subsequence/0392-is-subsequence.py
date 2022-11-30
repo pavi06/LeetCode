71ced44ddc20d6cf136ddd73213a2622282dbbd9
Pavithra P <69066762+pavi06@users.noreply.github.com>
2022-11-30 08:49:32 +0530

    Time: 70 ms (24.30%), Space: 13.9 MB (82.01%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,8 +1,8 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i,j=0,0
+        i,j=0,0   #making use of two pointer and iterate over the strings
         while i<len(s) and j<len(t):
-            if s[i]==t[j]:
+            if s[i]==t[j]:   #if both strings are ssame increment i and j ,if not increment j alone
                 i+=1
             j+=1
         if i==len(s):
