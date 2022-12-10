7cd2178b935d39e268eaf44abe3c562079cb3b96
Pavithra P <69066762+pavi06@users.noreply.github.com>
2022-12-10 10:23:42 +0530

    Time: 424 ms (73.97%), Space: 15.9 MB (53.31%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,9 +1,25 @@
-from itertools import combinations 
+# from itertools import combinations 
+
+#backstrack --->  decision tree
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        l=[]
-        for i in range(1,n+1):
-            l.append(i)
-        c=combinations(l,k)
-        return c
+        # l=[]
+        # for i in range(1,n+1):
+        #     l.append(i)
+        # c=combinations(l,k)
+        # return c
+        
+        res=[] #list to append the combinations
+        def backtracknum(start,comb):
+            if len(comb)==k:
+                res.append(comb.copy()) #appending the combination if it attains its maxlen : 'K'
+                return
+            
+            for i in range(start,n+1):
+                comb.append(i)   #adding the element to the list
+                backtracknum(i+1,comb)    #recursive call to check and add element
+                comb.pop()       #after the comb is found, remove the last element to make another comb. 
+          
+        backtracknum(1,[])
+        return res
