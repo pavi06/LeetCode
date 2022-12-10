2d81576ef7bcc0aef88c0a6e04d98be354e99ba1
Pavithra P <69066762+pavi06@users.noreply.github.com>
2022-12-10 23:56:41 +0530

    Time: 132 ms (96.29%), Space: 17.6 MB (31.53%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,14 +1,16 @@
 class Solution:
     def maxAreaOfIsland(self, grid):
         m, n = len(grid), len(grid[0])
-        neibs = [[1,0],[-1,0],[0,-1],[0,1]]
+        ns = [[1,0],[-1,0],[0,-1],[0,1]]
         islands, count = Counter(), 0
         
         def dfs(t, i, j):
-            if not 0<=i<m or not 0<=j<n or grid[i][j] != 1: return
+            if not 0<=i<m or not 0<=j<n or grid[i][j] != 1: 
+                return
             islands[t] += 1
             grid[i][j] = '#'
-            for x, y in neibs: dfs(t, x+i, y+j)
+            for x, y in ns: 
+                dfs(t, x+i, y+j)
         
         for i, j in product(range(m), range(n)):
             if grid[i][j] == 1:
