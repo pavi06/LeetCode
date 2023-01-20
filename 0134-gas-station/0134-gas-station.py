ddea0b8b68064ff2c903082993e94ef08537aca8
Pavithra P <69066762+pavi06@users.noreply.github.com>
2023-01-20 19:19:38 +0530

    Time: 1065 ms (11.42%), Space: 19.7 MB (14.74%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -6,11 +6,9 @@ class Solution:
         if total<0:                   #if total cost is less then return -1
             return -1
         rem=ind=0
-        for i in range(len(gas)):     #start from 0 index and check it is feasible to travel
-            currgas=gas[i]+rem        #each time the gas and rem is added
-            currcost=cost[i]          #cost is assigned to currcost var
-            rem=currgas-currcost        #cal remainder
-            if rem<0:                   #if rem less than 0 then move to next indx
+        for i in range(len(gas)):       #start from 0 index and check it is feasible to travel
+            rem=(gas[i]+rem)-cost[i]    #cal remainder by adding gas and rem and sub cost
+            if rem<0:                   #if rem less than 0 then move to next index
                 rem=0
                 ind=i+1
         return ind                   #return the index 
