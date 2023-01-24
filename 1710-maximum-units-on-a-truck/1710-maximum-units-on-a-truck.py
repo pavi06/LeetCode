db3c92b0ce7d0eb2a34aff563dedcd404ca78746
Pavithra P <69066762+pavi06@users.noreply.github.com>
2023-01-24 20:32:29 +0530

    Time: 234 ms (44.62%), Space: 14.5 MB (27.38%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,15 +1,14 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         total=0
-        boxTypes.sort(key=lambda x:-x[1])
+        boxTypes.sort(key=lambda x:-x[1]) #sorting the ele based on units and specify with minus to indicate desc order
         for i in range(len(boxTypes)):
             if truckSize>0:
                 if boxTypes[i][0] < truckSize:
                     total+=boxTypes[i][0]*boxTypes[i][1]
-                    print(total)
                     truckSize-=boxTypes[i][0]
                 else:
-                    total+=truckSize*boxTypes[i][1]
+                    total+=truckSize*boxTypes[i][1]    #if truckSize is lessthan  boxSize
                     break
         return total
         
