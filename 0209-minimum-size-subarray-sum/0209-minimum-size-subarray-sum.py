e8d3bd4382104179f3959bfcfe5225a1391a9d06
Pavithra P <69066762+pavi06@users.noreply.github.com>
2023-04-15 09:31:26 +0530

    Time: 264 ms (48.72%), Space: 27.1 MB (34.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,15 +1,15 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         l,r=0,0
-        minsize=float('inf')
+        minsize=float('inf')  #assigning the max value to the minsize var
         total=nums[0]
         while(r<len(nums) and l<=r):
-            if total>=target:
-                minsize=min(minsize,r-l+1)
+            if total>=target:  #if total is higher than target then incrementing the left pointer by 1
+                minsize=min(minsize,r-l+1)   #updating the minsize var
                 total-=nums[l]
                 l+=1
             else:
-                r+=1
+                r+=1     #if total is lesser than target then incrementing the right pointer by 1
                 if r<len(nums):
                     total+=nums[r]
             
