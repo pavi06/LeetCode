7e89038ba6b21fea16cd7021851bdaffabc4ee2e
Pavithra P <69066762+pavi06@users.noreply.github.com>
2023-04-15 11:31:58 +0530

    Time: 117 ms (55.96%), Space: 15.4 MB (45.32%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.py
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -2,7 +2,7 @@ class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         ma=max(nums)-k
         mi=min(nums)+k
-        if mi>ma:
+        if mi>ma:  #whenever this is true we can make ele same val thus resultant will be 0
             return 0
-        else:
+        else:     #simply return the diff
             return ma-mi
