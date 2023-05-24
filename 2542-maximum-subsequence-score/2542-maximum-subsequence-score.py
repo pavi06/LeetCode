c4fd6e89552f7eac82b49f69a8475e1d06fe27fd
Pavithra P <69066762+pavi06@users.noreply.github.com>
2023-05-24 10:37:34 +0530

    Time: 1255 ms (11.83%), Space: 37.4 MB (64.89%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -2,18 +2,18 @@ class Solution:
     def maxScore(self, nums1, nums2, k):
         total = res = 0
         heap = []
-        
+        #default min heap
         zip_pairs = zip(nums1, nums2)#paired each ele of two list
         sorted_pairs = sorted(zip_pairs, key=lambda x: -x[1])#sorted based on nums2
         for i in sorted_pairs:
             print(i)
-        for pair in sorted_pairs:
+        for pair in sorted_pairs:#iterate to all pairs and find max
             num1, num2 = pair  
             heappush(heap, num1)
             total += num1
-            if len(heap) > k:
+            if len(heap) > k: #if len > k means pop the first ele (i.e min ele)
                 total -= heappop(heap)
-            if len(heap) == k:
-                res = max(res, total * num2)
+            if len(heap) == k: #checking for max val if len==k
+                res = max(res, total * num2) #total holds the sum of k nums1 ele
         
         return res
